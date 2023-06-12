edcbc79b995934d0b5fa8b376736131faee1446e
MariaClaraBermudez <mariaclarabermudez10@gmail.com>
2023-06-12 13:11:00 -0300

    criei js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Mandando oi para o amigo!");
+
+function mandaMensagem() {
+    console.log("Tudo Bem ?");
+    console.log("Vou te mandar uma solicitação");
+    console.log("Solicitação recebida!");
+
+}
+
+mandaMensagem();
+
+console.log("Tchau tchau !");
